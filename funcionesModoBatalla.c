0d73361235b4e9c0797f6653b90cfc6a03788f49
Adrián Teixeira de Uña <adrian.teixeira@alumnos.upm.es>
2021-04-28 17:18:56 +0200

    Pulir código

--- funcionesModoBatalla.c
+++ funcionesModoBatalla.c
@@ -2,15 +2,15 @@
 #define NECESARIO_R 25
 #define FACTOR_R 0.75
 
-float calculoDificultad(void) //AVISO: PROVISIONAL
-{
-    float factor;
-    float r = 0.1;
-    puntuacionGlobal global; //en realidad hay que acceder a un fichero con las puntuaciones totales
-    if (global.totalPartidas < NECESARIO_R)      //esto se hace para que cuando se hayan jugado pocas partidas no se vuelva muy dificil el juego, aumentando progresivamente
-        r = global.totalPartidas/10;    //hasta que se hayan jugado ciertas partidas. Es entonces cuando la dificultad global aumenta segun el ratio de
-    else                                //partidas ganadas y partidas jugadas
-        r = FACTOR_R;
-    factor = 1+r*global.totalGanadas/global.totalPartidas;
-    return factor;
-}
+//float calculoDificultad(void) //AVISO: PROVISIONAL
+//{
+//    float factor;
+//    float r = 0.1;
+//    puntuacionGlobal global; //en realidad hay que acceder a un fichero con las puntuaciones totales
+//    if (global.totalPartidas < NECESARIO_R)      //esto se hace para que cuando se hayan jugado pocas partidas no se vuelva muy dificil el juego, aumentando progresivamente
+//        r = global.totalPartidas/10;    //hasta que se hayan jugado ciertas partidas. Es entonces cuando la dificultad global aumenta segun el ratio de
+//    else                                //partidas ganadas y partidas jugadas
+//        r = FACTOR_R;
+//    factor = 1+r*global.totalGanadas/global.totalPartidas;
+//    return factor;
+//}
--- menu_principal.c
+++ menu_principal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "funcionesTexto.h"
 #include "estructuras.h"
@@ -8,41 +9,57 @@ int main()
     char volver[6];
     const char atras[6] = "atras", condjugar[6] = "jugar";
     int condicion, dificultad;
-    start:
-        condicion = inicioPrograma(condicion);
-        switch (condicion) {
-        case 1:
-            printf("Has seleccionado personalizar barco.\n");
-            scanf("%s", volver);
-            if (strcmp(volver, atras) == 0) {
-                strcpy(volver, "00000");
-                goto start;
+    start: {
+        while (strcmp(volver, atras) != 0)
+        {
+            system("cls");
+            condicion = inicioPrograma(condicion);
+            switch (condicion) {
+            case 1:
+                system("cls");
+                printf("Has seleccionado personalizar barco.\n");
+                scanf("%s", volver);
+                break;
+
+            case 2:
+                system("cls");
+                printf("Has seleccionado Jugar.\n");
+                dificultad = jugar();
+                if (dificultad == 555)
+                    goto start;
+                switch (dificultad)
+                {
+                case 555:
+                    goto start;
+                    break;
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 3:
+                    break;
+                }
+                break;
+
+            case 3:
+                system("cls");
+                printf("Has seleccionado Puntuacion. Escribe 'v' para ver la puntuacion.\n");
+                scanf("%s", volver);
+                if (strcmp(volver, "v") == 0)
+                    system("\"puntuacion.csv\"");
+                break;
+
+            default:
+                system("cls");
+                error();
+                goto end;
+                break;
             }
-            break;
-
-        case 2:
-            printf("Has seleccionado Jugar.\n");
-            dificultad = jugar();
-            if (dificultad == 555)
-                goto start;
-            //fopen();
-            break;
-
-        case 3:
-            printf("Has seleccionado Puntuacion.\n");
-            scanf("%s", volver);
-            if (strcmp(volver, atras) == 0) {
-                strcpy(volver, "00000");
-                goto start;
-            }
-            //abrir otro archivo con la puntuación
-            break;
-
-        default:
-            error();
-            goto end;
-            break;
         }
+    }
+
+    modoBatalla:
+
 
     end:
         return 0;

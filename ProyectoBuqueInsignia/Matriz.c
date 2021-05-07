16e9402046b53e05fbd7566e6db07959f6689763
Jorge Bachiller Cordero <jorge.bachiller.cordero@alumnos.upm.es>
2021-05-07 12:13:34 +0200

    Error

---
+++ ProyectoBuqueInsignia/Matriz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Matriz.h>
+
+int Matriz(int x) {
+    int misBarcos[10][10] = {{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}};
+    int cpuBarcos[10][10] = {{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0,0}};
+
+    rellenarTodosLosBarcos(misBarcos);
+    rellenarTodosLosBarcos(cpuBarcos);
+
+    imprimirMatrizdeBarcos(misBarcos, 10, 10);
+    return 0;
+}
+void imprimirMatrizdeBarcos(int MatBarcos[][10], int fila, int column) {
+    printf("- A B C D E F G H I J\n");
+    for (int x=0; x<fila; x++) {
+        printf("%d ", x);
+        for(int y=0; y<column; y++) {
+            printf("%d ", MatBarcos[x][y]);
+        }
+        printf("\n");
+    }
+}
